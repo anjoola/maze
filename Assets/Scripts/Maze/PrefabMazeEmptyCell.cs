7fd68168b3611454bc2a7f628b14c7b3d09d2cae
Angela Gong <anjoola@anjoola.com>
2015-07-03 01:17:29 -0700

    Added game UI

---
+++ Assets/Scripts/Maze/PrefabMazeEmptyCell.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+/**
+ * Represents an empty cell in the maze. Used for enemy spawning.
+ */
+public class EmptyCell {
+	// Size of a maze block.
+	int MAZE_BLOCK_SIZE = 1000;
+	
+	int MazeCenter;
+	Vector3 Coordinates;
+
+	// Properties of this cell. Mapping from direction -> magnitude. This represents the number of free spaces in that
+	// direction.
+	int[] Properties;
+	public int NumPropertiesSet;
+	
+	public EmptyCell(int size, int row, int col) {
+		MazeCenter = size / 2 + 1;
+		Coordinates = new Vector3((col - MazeCenter) * MAZE_BLOCK_SIZE, 0, (row - MazeCenter) * MAZE_BLOCK_SIZE);
+
+		Properties = new int[4];
+		NumPropertiesSet = 0;
+
+		// If this is an edge cell, set properties we already know.
+		if (row == 1)
+			SetProperty(EmptyCellOpenDirection.BELOW, 0);
+		if (col == 1)
+			SetProperty(EmptyCellOpenDirection.LEFT, 0);
+		if (row == size - 1)
+			SetProperty(EmptyCellOpenDirection.ABOVE, 0);
+		if (col == size - 1)
+			SetProperty(EmptyCellOpenDirection.RIGHT, 0);
+	}
+
+	/**
+	 * Set a property of the cell. Indicates in which directions it is empty.
+	 * 
+	 * direction: Direction of emptiness.
+	 * magnitude: The number of cells it is empty for.
+	 */
+	public void SetProperty(EmptyCellOpenDirection direction, int magnitude) {
+		Properties[(int) direction] = magnitude;
+		NumPropertiesSet++;
+	}
+}
+
+/**
+ * Direction of emptiness, relative to the current maze block.
+ */
+public enum EmptyCellOpenDirection : int {
+	LEFT = 0, RIGHT, ABOVE, BELOW, ANY
+};
--- Assets/Scripts/Maze/PrefabMazeGen.cs
+++ Assets/Scripts/Maze/PrefabMazeGen.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 /**
  * Generates a maze using Prefabs. Need to add all the potential maze blocks to the MazeBlocks list and set the Size.
- * The MazeBlocks list size must be Size x Size.
+ * The MazeBlocks list size must be Size x Size, where Size is an odd number.
  */
 public class PrefabMazeGen : MonoBehaviour {
 	PrefabMazeGenerator Gen;
@@ -11,17 +12,75 @@ public class PrefabMazeGen : MonoBehaviour {
 	// Size of one side of the maze. (e.g. an 11 x 11 maze would have Size = 11).
 	public int Size;
 	public GameObject[] MazeBlocks;
-
+	
 	public class PrefabMazeGenerator : MazeGenerator {
 		public GameObject[] MazeBlocks;
 
+		// List of all empty spaces in the maze. An array of coordinates.
+		public EmptyCell[] EmptySpaces;
+
+		// Maze size must be an odd number.
 		public PrefabMazeGenerator(GameObject[] blocks, int size) : base(size) {
 			MazeBlocks = blocks;
+
+			// Initialize empty cells.
+			EmptySpaces = new EmptyCell[size * size];
+			for (int row = 1; row < size - 1; row++) {
+				for (int col = 1; col < size - 1; col++) {
+					// The first and last cells are where the player the goal are, respectively.
+					if ((row == 1 && col == 1) || (row == size - 1 && col == size - 1))
+						continue;
+
+					EmptySpaces[row * size + col] = new EmptyCell(size, row, col);
+				}
+			}
 		}
 
 		override public void CellTypeChangedCallback(int row, int col, bool toWall) {
+			// Changed from a wall to an empty space.
 			if (!toWall && this.MazeBlocks[row * this.Size + col] != null) {
-				this.MazeBlocks[row * this.Size + col].SetActive(false);
+				this.MazeBlocks[row * Size + col].SetActive(false);
+
+				// Add it to the list of empty spaces.
+				EmptySpaces[row * Size + col] = new EmptyCell(Size, row, col);
+			}
+		}
+
+		/**
+		 * Compute properties for each empty cell in the maze (used for getting enemy spawn locations).
+		 */
+		public void ComputeEmptyCells() {
+			int row = 1;
+			int col = 1;
+			while (true) {
+				EmptyCell cell = EmptySpaces[row * Size + col];
+				if (cell != null) {
+
+
+
+
+				int nextCol = row + 1;
+				int numSpaces = 1;
+				while (nextCol < Size - 1) {
+					EmptyCell nextCell = EmptySpaces[row * Size + nextCol];
+					if (nextCell != null) {
+						numSpaces++;
+						nextCol++;
+					}
+
+					else
+						break;
+				}
+
+				Debug.Log ("(" + row + "," + col + "): " + numSpaces);
+				}
+
+				if (++col > Size - 1) {
+					col = 1;
+					++row;
+				}
+				if (row > Size - 1)
+					break;
 			}
 		}
 	}
@@ -29,5 +88,6 @@ public class PrefabMazeGen : MonoBehaviour {
 	void Start() {
 		Gen = new PrefabMazeGenerator(MazeBlocks, Size);
 		Gen.GenerateMaze();
+		Gen.ComputeEmptyCells();
 	}
 }
