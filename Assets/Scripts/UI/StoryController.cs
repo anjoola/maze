e60e1506b54bf5ab3df55e0c8afe2b1303251120
Angela Gong <anjoola@anjoola.com>
2015-07-28 20:43:49 -0700

    Added animations to story

--- Assets/Scripts/UI/StoryController.cs
+++ Assets/Scripts/UI/StoryController.cs
@@ -3,9 +3,25 @@ using System.Collections;
 
 public class StoryController : MonoBehaviour {
 	public string NextScene;
-	
-	void Update() {
-		if (Input.GetMouseButtonDown(0))
-			AutoFade.LoadLevel(NextScene, 0.2f, 0.2f, Color.black);
+
+	public GameObject Image, Text;
+	public float Delay = 4;
+
+	void Start() {
+		iTween.MoveFrom(Text, iTween.Hash("y", -58, "easetype", "linear", "time", 0.4f, "delay", 0.1f));
+		iTween.MoveFrom(Image, iTween.Hash("y", 1, "easetype", "linear", "time", 0.7f, "delay", 0.1f));
+		
+		iTween.MoveBy(Text, iTween.Hash("y", -4, "loopType", "pingpong", "easetype", "linear", "time", 0.7));
+		iTween.MoveBy(Image, iTween.Hash("y", 0.008f, "looptype", "pingpong", "easetype", "linear", "time", 0.7f));
+		
+		iTween.MoveTo(Text, iTween.Hash("y", -58, "easetype", "linear", "time", 0.6f, "delay", Delay));
+		iTween.MoveTo(Image, iTween.Hash("y", 1, "easetype", "linear", "time", 0.7f, "delay", Delay));
+
+		StartCoroutine(Fade());
+	}
+
+	IEnumerator Fade() {
+		yield return new WaitForSeconds(Delay + 0.5f);
+		AutoFade.LoadLevel(NextScene, 0.1f, 0.1f, Color.black);
 	}
 }
